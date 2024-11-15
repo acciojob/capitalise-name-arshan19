b595152a823709e81c7937cce143d08bf61372a6
Arshan Ali <96035329+arshan19@users.noreply.github.com>
2024-11-15 15:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let ip = document.getElementById("fname")
+
+ip.addEventListener("blur", () =>{
+	ip.value = ip.value.toUpperCase()
+	// let temp = ip.value.toUpperCase()
+	// ip.value = temp
+})
